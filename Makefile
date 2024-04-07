e3ac99244f53ef05adff8bf38a9ccce12f8906bf
MATH-286-Pro <1299065753@qq.com>
2024-04-07 23:43:18 +0800

    [更新] 编译通过

--- Makefile
+++ Makefile
@@ -78,8 +78,6 @@ Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c \
 Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c \
 Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c \
 Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c \
-Core/Src/system_stm32f4xx.c \
-Core/Src/tim.c \
 Middlewares/Third_Party/FreeRTOS/Source/croutine.c \
 Middlewares/Third_Party/FreeRTOS/Source/event_groups.c \
 Middlewares/Third_Party/FreeRTOS/Source/list.c \
--- application/INS_task.c
+++ application/INS_task.c
@@ -4,11 +4,9 @@
   * @brief      use bmi088 to calculate the euler angle. no use ist8310, so only
   *             enable data ready pin to save cpu time.enalbe bmi088 data ready
   *             enable spi DMA to save the time spi transmit
-  * 
-  *             主要利用陀螺仪 bmi088, 磁力计 ist8310 完成姿态解算，得出欧拉角，
-  *             提供通过 BMI088 的 data ready 中断完成外部触发，减少数据等待延迟，
-  *             通过DMA的SPI传输节约CPU时间
-  * 
+  *             主要利用陀螺仪bmi088，磁力计ist8310，完成姿态解算，得出欧拉角，
+  *             提供通过bmi088的data ready 中断完成外部触发，减少数据等待延迟
+  *             通过DMA的SPI传输节约CPU时间.
   * @note       
   * @history
   *  Version    Date            Author          Modification
@@ -48,11 +46,11 @@
   * @retval         none
   */
 /**
-  * @brief          控制 bmi088 温度
-  * @param[in]      temp:bmi088 的温度
+  * @brief          控制bmi088的温度
+  * @param[in]      temp:bmi088的温度
   * @retval         none
   */
-static void imu_temp_control(float temp);
+static void imu_temp_control(fp32 temp);
 
 /**
   * @brief          open the SPI DMA accord to the value of imu_update_flag
@@ -67,9 +65,9 @@ static void imu_temp_control(float temp);
 static void imu_cmd_spi_dma(void);
 
 
-void AHRS_init(float quat[4], float accel[3], float mag[3]);
-void AHRS_update(float quat[4], float time, float gyro[3], float accel[3], float mag[3]);
-void get_angle(float quat[4], float *yaw, float *pitch, float *roll);
+void AHRS_init(fp32 quat[4], fp32 accel[3], fp32 mag[3]);
+void AHRS_update(fp32 quat[4], fp32 time, fp32 gyro[3], fp32 accel[3], fp32 mag[3]);
+void get_angle(fp32 quat[4], fp32 *yaw, fp32 *pitch, fp32 *roll);
 
 extern SPI_HandleTypeDef hspi1;
 
@@ -92,19 +90,18 @@ volatile uint8_t accel_temp_update_flag = 0;
 volatile uint8_t mag_update_flag = 0;
 volatile uint8_t imu_start_dma_flag = 0;
 
-//定义陀螺仪数据 类型
+
 bmi088_real_data_t bmi088_real_data;
-//定义磁力计数据 类型
 ist8310_real_data_t ist8310_real_data;
 
 
 static uint8_t first_temperate;
-static const float imu_temp_PID[3] = {TEMPERATURE_PID_KP, TEMPERATURE_PID_KI, TEMPERATURE_PID_KD};
+static const fp32 imu_temp_PID[3] = {TEMPERATURE_PID_KP, TEMPERATURE_PID_KI, TEMPERATURE_PID_KD};
 static pid_type_def imu_temp_pid;
 
 
-float INS_quat[4] = {0.0f, 0.0f, 0.0f, 0.0f};
-float INS_angle[3] = {0.0f, 0.0f, 0.0f};      //euler angle, unit rad.欧拉角 单位 rad
+fp32 INS_quat[4] = {0.0f, 0.0f, 0.0f, 0.0f};
+fp32 INS_angle[3] = {0.0f, 0.0f, 0.0f};      //euler angle, unit rad.欧拉角 单位 rad
 
 
 
@@ -118,11 +115,9 @@ float INS_angle[3] = {0.0f, 0.0f, 0.0f};      //euler angle, unit rad.欧拉角 
   * @param[in]      pvParameters: NULL
   * @retval         none
   */
-//定义RTOS任务
 void INS_task(void const *pvParameters)
 {
     //wait a time
-    //等待启动
     osDelay(INS_TASK_INIT_TIME);
     while(BMI088_init())
     {
@@ -133,17 +128,15 @@ void INS_task(void const *pvParameters)
         osDelay(100);
     }
 
-    //通过函数将 陀螺仪数据 写入变量bmi088_real_data
     BMI088_read(bmi088_real_data.gyro, bmi088_real_data.accel, &bmi088_real_data.temp);
 
     PID_init(&imu_temp_pid, PID_POSITION, imu_temp_PID, TEMPERATURE_PID_MAX_OUT, TEMPERATURE_PID_MAX_IOUT);
 
-    //初始位置 设定
     AHRS_init(INS_quat, bmi088_real_data.accel, ist8310_real_data.mag);
 
 
     //get the handle of task
-    //获取当前任务的任务句柄
+    //获取当前任务的任务句柄，
     INS_task_local_handler = xTaskGetHandle(pcTaskGetName(NULL));
 
     //set spi frequency
@@ -187,32 +180,28 @@ void INS_task(void const *pvParameters)
             imu_temp_control(bmi088_real_data.temp);
         }
 
-        //位置更新
+
         AHRS_update(INS_quat, 0.001f, bmi088_real_data.gyro, bmi088_real_data.accel, ist8310_real_data.mag);
-        //获取角度 YAW,PITCH,ROLL
         get_angle(INS_quat, INS_angle + INS_YAW_ADDRESS_OFFSET, INS_angle + INS_PITCH_ADDRESS_OFFSET, INS_angle + INS_ROLL_ADDRESS_OFFSET);
 
 
     }
 }
 
-//位置初始化 = 设定初始位置
-void AHRS_init(float quat[4], float accel[3], float mag[3])
+void AHRS_init(fp32 quat[4], fp32 accel[3], fp32 mag[3])
 {
     quat[0] = 1.0f;
     quat[1] = 0.0f;
     quat[2] = 0.0f;
     quat[3] = 0.0f;
+
 }
 
-//位置更新
-void AHRS_update(float quat[4], float time, float gyro[3], float accel[3], float mag[3])
+void AHRS_update(fp32 quat[4], fp32 time, fp32 gyro[3], fp32 accel[3], fp32 mag[3])
 {
     MahonyAHRSupdate(quat, gyro[0], gyro[1], gyro[2], accel[0], accel[1], accel[2], mag[0], mag[1], mag[2]);
 }
-
-//获取三轴角度 YAW,PITCH,ROLL
-void get_angle(float q[4], float *yaw, float *pitch, float *roll)
+void get_angle(fp32 q[4], fp32 *yaw, fp32 *pitch, fp32 *roll)
 {
     *yaw = atan2f(2.0f*(q[0]*q[3]+q[1]*q[2]), 2.0f*(q[0]*q[0]+q[1]*q[1])-1.0f);
     *pitch = asinf(-2.0f*(q[1]*q[3]-q[0]*q[2]));
@@ -225,11 +214,11 @@ void get_angle(float q[4], float *yaw, float *pitch, float *roll)
   * @retval         none
   */
 /**
-  * @brief          ����bmi088���¶�
-  * @param[in]      temp:bmi088���¶�
+  * @brief          控制bmi088的温度
+  * @param[in]      temp:bmi088的温度
   * @retval         none
   */
-static void imu_temp_control(float temp)
+static void imu_temp_control(fp32 temp)
 {
     uint16_t tempPWM;
     static uint8_t temp_constant_time = 0;
@@ -245,14 +234,14 @@ static void imu_temp_control(float temp)
     }
     else
     {
-        //��û�дﵽ���õ��¶ȣ�һֱ����ʼ���
+        //在没有达到设置的温度，一直最大功率加热
         //in beginning, max power
         if (temp > 45.0f)
         {
             temp_constant_time++;
             if (temp_constant_time > 200)
             {
-                //�ﵽ�����¶ȣ�������������Ϊһ������ʣ���������
+                //达到设置温度，将积分项设置为一半最大功率，加速收敛
                 //
                 first_temperate = 1;
                 imu_temp_pid.Iout = MPU6500_TEMP_PWM_MAX / 2.0f;
@@ -298,7 +287,7 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
     else if(GPIO_Pin == GPIO_PIN_0)
     {
         //wake up the task
-        //��������
+        //唤醒任务
         if (xTaskGetSchedulerState() != taskSCHEDULER_NOT_STARTED)
         {
             static BaseType_t xHigherPriorityTaskWoken;
@@ -316,8 +305,8 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
   * @retval         none
   */
 /**
-  * @brief          ����imu_update_flag��ֵ����SPI DMA
-  * @param[in]      temp:bmi088���¶�
+  * @brief          根据imu_update_flag的值开启SPI DMA
+  * @param[in]      temp:bmi088的温度
   * @retval         none
   */
 static void imu_cmd_spi_dma(void)
@@ -325,7 +314,7 @@ static void imu_cmd_spi_dma(void)
     UBaseType_t uxSavedInterruptStatus;
     uxSavedInterruptStatus = taskENTER_CRITICAL_FROM_ISR();
 
-    //���������ǵ�DMA����
+    //开启陀螺仪的DMA传输
     if( (gyro_update_flag & (1 << IMU_DR_SHFITS) ) && !(hspi1.hdmatx->Instance->CR & DMA_SxCR_EN) && !(hspi1.hdmarx->Instance->CR & DMA_SxCR_EN)
     && !(accel_update_flag & (1 << IMU_SPI_SHFITS)) && !(accel_temp_update_flag & (1 << IMU_SPI_SHFITS)))
     {
@@ -337,7 +326,7 @@ static void imu_cmd_spi_dma(void)
         taskEXIT_CRITICAL_FROM_ISR(uxSavedInterruptStatus);
         return;
     }
-    //�������ٶȼƵ�DMA����
+    //开启加速度计的DMA传输
     if((accel_update_flag & (1 << IMU_DR_SHFITS)) && !(hspi1.hdmatx->Instance->CR & DMA_SxCR_EN) && !(hspi1.hdmarx->Instance->CR & DMA_SxCR_EN)
     && !(gyro_update_flag & (1 << IMU_SPI_SHFITS)) && !(accel_temp_update_flag & (1 << IMU_SPI_SHFITS)))
     {
@@ -376,7 +365,7 @@ void DMA2_Stream2_IRQHandler(void)
         __HAL_DMA_CLEAR_FLAG(hspi1.hdmarx, __HAL_DMA_GET_TC_FLAG_INDEX(hspi1.hdmarx));
 
         //gyro read over
-        //�����Ƕ�ȡ���
+        //陀螺仪读取完毕
         if(gyro_update_flag & (1 << IMU_SPI_SHFITS))
         {
             gyro_update_flag &= ~(1 << IMU_SPI_SHFITS);
@@ -387,7 +376,7 @@ void DMA2_Stream2_IRQHandler(void)
         }
 
         //accel read over
-        //���ٶȼƶ�ȡ���
+        //加速度计读取完毕
         if(accel_update_flag & (1 << IMU_SPI_SHFITS))
         {
             accel_update_flag &= ~(1 << IMU_SPI_SHFITS);
@@ -396,7 +385,7 @@ void DMA2_Stream2_IRQHandler(void)
             HAL_GPIO_WritePin(CS1_ACCEL_GPIO_Port, CS1_ACCEL_Pin, GPIO_PIN_SET);
         }
         //temperature read over
-        //�¶ȶ�ȡ���
+        //温度读取完毕
         if(accel_temp_update_flag & (1 << IMU_SPI_SHFITS))
         {
             accel_temp_update_flag &= ~(1 << IMU_SPI_SHFITS);
